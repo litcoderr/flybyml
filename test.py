5a2614f9ebe3c3049152fd9b4b1ca33c6ed38789
Youngchae Chee <litcoderr@gmail.com>
2023-09-07 19:38:33 +0900

    update

--- test.py
+++ test.py
@@ -2,12 +2,17 @@ from action import getPos, setPos
 from pos import Gimpo
 from att import CessnaInit
 
-"""
+import time
+from dref import hasCrashed, setIndicatedAirspeed, getIndicatedAirspeed, sendDref, getDref
+
 pos, att, gear = getPos()
 print(pos)
 print(att)
-"""
+pos.alt = 100
 
-pos = Gimpo()
-att = CessnaInit(320)
 setPos(pos, att, 1)
+
+while True:
+    print(hasCrashed())
+    print(getIndicatedAirspeed())
+    time.sleep(1)
