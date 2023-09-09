1f5f421fa078df1641a03cc6734f4dbe2a0f9484
Youngchae Chee <litcoderr@gmail.com>
2023-09-09 14:52:22 +0900

    initial version of udp thread control of getting dref from xplane server

--- test.py
+++ test.py
@@ -1,18 +1,36 @@
-from action import getPos, setPos
-from pos import Gimpo
-from att import CessnaInit
+from connect import XP
 
-import time
-from dref import hasCrashed, setIndicatedAirspeed, getIndicatedAirspeed, sendDref, getDref
-
-pos, att, gear = getPos()
-print(pos)
-print(att)
-pos.alt = 100
-
-setPos(pos, att, 1)
+xp = XP()
 
 while True:
-    print(hasCrashed())
-    print(getIndicatedAirspeed())
-    time.sleep(1)
+    # TODO test getting dref
+    pass
+
+"""
+beacon = find_xp()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 1) Subscribe to receive once per second
+cmd = b'RREF'  # "Request DataRef(s)"
+freq = 1       # number of times per second (integer)
+index = 0      # "my" number, so I can match responsed with my request
+msg = struct.pack("<4sxii400s", cmd, freq, index, b'sim/aircraft/engine/acf_num_engines')
+sock.sendto(msg, (beacon['ip'], beacon['port']))
+
+# 2) Block, waiting to receive a packet
+data, addr = sock.recvfrom(2048)
+header = data[0:4]
+if header[0:4] != b'RREF':
+    raise ValueError("Unknown packet")
+
+# 3) Unpack the data:
+idx, value = struct.unpack("<if", data[5:13])
+assert idx == index
+print("Number of engines is {}".format(int(value)))
+
+# 4) Unsubscribe -- as otherwise we'll continue to get this data, once every second!
+freq = 0
+msg = struct.pack("<4sxii400s", cmd, freq, index, b'sim/aircraft/engine/acf_num_engines')
+sock.sendto(msg, (beacon['ip'], beacon['port']))
+sock.close()
+"""
